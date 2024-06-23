9044ddcb4464305d6612ca78d68f5be55e6d0514
dzinais.kaliada <dvkoleda@gmail.com>
2024-06-23 18:16:37 +0200

    Added comments regarding game logic changes

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,19 +1,22 @@
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+// Rename to geme table
 public class Circle {
-	private final int numCells;
+	private final int numCells;// no need for this field just use List of Objects/Frog/Or list of generics
 	private final Lock lock = new ReentrantLock();
 
 	public Circle(int numCells) {
 		this.numCells = numCells;
 	}
 
-	public Lock getLock() {
-		return lock;
-	}
+	//Add method move that will use lock to move frog from i to j
 
-	public int getNumCells() {
-		return numCells;
-	}
+//	public Lock getLock() {
+//		return lock;
+//	}
+//
+//	public int getNumCells() {
+//		return numCells;
+//	}
 }
--- src/main/java/Frog.java
+++ src/main/java/Frog.java
@@ -1,7 +1,9 @@
 public class Frog implements Runnable {
 	private final int frogIndex;
+	//add name of the frog
 	private final int speed;
 	private int position;
+	//Frog should/May know about GameTable
 	private boolean finished;
 
 	public Frog(int frogIndex, int speed) {
@@ -35,8 +37,13 @@ public class Frog implements Runnable {
 
 	@Override
 	public void run() {
+
+		// Some one should start this thread!
 		while (!finished) {
 			try {
+				// Call to GameTable move method.
+				// Print something like "Frog With name moved from i to j
+				// Or frog With Name failed to move from i to j as J is busy.
 				Thread.sleep(1000);
 				jump();
 			} catch (InterruptedException e) {
